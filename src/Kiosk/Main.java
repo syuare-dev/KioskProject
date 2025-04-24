6eb7cb78e35539aacd5ebaec050279dfaa41e9b7
syuare-dev <tachunsa13@gmail.com>
2025-04-24 13:33:16 +0900

    Init: 초기 설정

---
+++ src/Kiosk/Main.java
@@ -0,0 +1,13 @@
+package Kiosk;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 테스트 코드
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
