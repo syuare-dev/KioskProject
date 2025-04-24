872ce072e2de032fb89c0f69905d7a55628a6721
syuare-dev <tachunsa13@gmail.com>
2025-04-24 13:33:52 +0900

    Init: 초기 설정

--- src/Kiosk/Main.java
+++ src/Kiosk/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // 테스트 코드
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
