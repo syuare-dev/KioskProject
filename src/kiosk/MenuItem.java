e56f4c82c1b1805653a854834b1a8e9ba793bc1f
syuare-dev <tachunsa13@gmail.com>
2025-04-24 17:24:55 +0900

    Add: MenuItem 속성, getter 구현

---
+++ src/kiosk/MenuItem.java
@@ -0,0 +1,28 @@
+package kiosk;
+
+public class MenuItem {
+
+    // 속성
+    private final String burger;
+    private final double price;
+    private final String description;
+
+    // 생성자
+    MenuItem (String burger, double price, String description) {
+        this.burger = burger;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getBurger() {
+        return burger;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
