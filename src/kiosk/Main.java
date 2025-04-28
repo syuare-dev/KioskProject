efc2cfe4100fc90a480c0b64d3e022881adadd74
syuare-dev <tachunsa13@gmail.com>
2025-04-28 17:59:48 +0900

    Add: 음료 메뉴 추가

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -1,5 +1,7 @@
 package kiosk;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,18 +9,31 @@ public class Main {
         // 메뉴 객체
         Menu menu = new Menu();
 
+        MenuItem double1955 = new MenuItem("Double 1955", 10.3, "113g 두툼한 순 쇠고기 패티, 특별한 1955 소스에 깊은 풍미의 그릴드 어니언까지", "Burger");
+        MenuItem bulgogiBurger = new MenuItem("Bulgogi Burger", 4.4, "100% 순 쇠고기 패티 두 장에 빅맥®만의 특별한 소스. 입안에서 살살 녹는 치즈와 신선한 양상추, 양파, 그리고 피클까지", "Burger");
+        MenuItem doubleCheeseburger = new MenuItem("Double Cheeseburger", 5.6, "고소하고 부드러운 치즈와 100% 순 쇠고기 패티가 두개, 맥도날드만의 더블 치즈버거!!", "Burger");
+        MenuItem mcChicken = new MenuItem("McChicken", 4.3, "바삭한 치킨 패티, 고소한 화이트 마요 소스와 아삭한 양상추가 함께! 더 업그레이드된 맛", "Burger");
 
-        MenuItem double1955 = new MenuItem("Double 1955", 10.3, "113g 두툼한 순 쇠고기 패티, 특별한 1955 소스에 깊은 풍미의 그릴드 어니언까지");
-        MenuItem bulgogiBurger = new MenuItem("Bulgogi Burger", 4.4, "100% 순 쇠고기 패티 두 장에 빅맥®만의 특별한 소스. 입안에서 살살 녹는 치즈와 신선한 양상추, 양파, 그리고 피클까지");
-        MenuItem doubleCheeseburger = new MenuItem("Double Cheeseburger", 5.6, "고소하고 부드러운 치즈와 100% 순 쇠고기 패티가 두개, 맥도날드만의 더블 치즈버거!!");
-        MenuItem mcChicken = new MenuItem("McChicken", 4.3, "바삭한 치킨 패티, 고소한 화이트 마요 소스와 아삭한 양상추가 함께! 더 업그레이드된 맛");
+        MenuItem cocaCola = new MenuItem("Coca-Cola", 3.1, "갈증해소 뿐만이 아니라 기분까지 상쾌하게! 코카-콜라","Drink");
+        MenuItem sprite = new MenuItem("Sprite", 3.1, "청량함에 레몬, 라임향을 더한 시원함!","Drink");
+        MenuItem peachIcedTea = new MenuItem("Peach Iced Tea", 3.6, "세계 3대 홍차, 우바산 홍차 베이스로 향긋하게 즐기는 복숭아 아이스티","Drink");
+        MenuItem americano = new MenuItem("Americano", 3.1, "진한 에스프레소 샷에 따듯한 물을 섞어 더 향긋하게 즐기는 정통 아메리카노","Drink");
 
-        menu.addMenu(double1955);
-        menu.addMenu(bulgogiBurger);
-        menu.addMenu(doubleCheeseburger);
-        menu.addMenu(mcChicken);
 
-        menu.menuBoardBurger();
+        // 객체 데이터 추가
+        for (MenuItem menuItem : Arrays.asList(
+                double1955,
+                bulgogiBurger,
+                doubleCheeseburger,
+                mcChicken,
+                cocaCola,
+                sprite,
+                peachIcedTea,
+                americano)) {
+            menu.addMenu(menuItem);
+        }
+
+        // menu.menuBoardBurger();
         Kiosk kiosk = new Kiosk(menu);
         kiosk.startKiosk();
 
