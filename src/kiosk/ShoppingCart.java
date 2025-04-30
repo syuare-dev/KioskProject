9b801ef6b99b7bc2832f224907e13b8e0b171625
syuare-dev <tachunsa13@gmail.com>
2025-04-30 17:54:30 +0900

    Add: 장바구니에 추가된 메뉴들의 금액 총합이 출력되도록 구현

--- src/kiosk/ShoppingCart.java
+++ src/kiosk/ShoppingCart.java
@@ -22,15 +22,26 @@ public class ShoppingCart {
     }
 
     public void checkCart() {
+        int num =1;
         System.out.println("==========장바구니 조회==========");
 
         if(getShoppingCart().isEmpty()){ // 장바구니가 비어있을 경우 출력
             System.out.println("장바구니가 비어있습니다.");
         } else { // 장바구니에 담긴 메뉴가 있을 경우 출력
             for (MenuItem list : getShoppingCart()) {
-                System.out.println(list);
+                System.out.println(num + ". " + list);
+                num++;
             }
         }
+        System.out.printf("- 총 금액: W %-3.1f\n",totalPrice());
+    }
+
+    public double totalPrice() {
+        double totalPrice = 0;
+        for(MenuItem tp : getShoppingCart()) {
+            totalPrice += tp.getPrice();
+        }
+        return totalPrice;
     }
 
 }
