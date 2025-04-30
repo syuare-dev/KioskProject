797514fe6f3b34bf1357e5111c35138a340069d8
syuare-dev <tachunsa13@gmail.com>
2025-04-30 19:41:06 +0900

    Add: removeCart 구현(장바구니 내 모든 값 삭제)

--- src/kiosk/ShoppingCart.java
+++ src/kiosk/ShoppingCart.java
@@ -3,39 +3,42 @@ package kiosk;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ShoppingCart {
 
-    private final List<MenuItem> shoppingCart = new ArrayList<>(); // 장바구니 List 초기화
+    private final List<MenuItem> shoppingCart = new ArrayList<>();
 
     // 생성자
 
+
+    // 기능(메서드)
+
+    // 장바구니에 메뉴 추가
     public void addToCart(MenuItem menuItem) {
         shoppingCart.add(menuItem);
     }
 
-    public void removeInCart(MenuItem menuItem){
-        shoppingCart.remove(menuItem);
+    // 장바구니에 포함된 메뉴 삭제
+    public void removeCart(){
+        shoppingCart.clear();
     }
 
     public List<MenuItem> getShoppingCart() {
         return new ArrayList<>(shoppingCart);
     }
 
+    // 장바구니 조회
     public void checkCart() {
         int num =1;
         System.out.println("==========장바구니 조회==========");
-
-        if(getShoppingCart().isEmpty()){ // 장바구니가 비어있을 경우 출력
-            System.out.println("장바구니가 비어있습니다.");
-        } else { // 장바구니에 담긴 메뉴가 있을 경우 출력
-            for (MenuItem list : getShoppingCart()) {
-                System.out.println(num + ". " + list);
-                num++;
-            }
+        for (MenuItem list : getShoppingCart()) {
+            System.out.println(num + ". " + list);
+            num++;
         }
         System.out.printf("- 총 금액: W %-3.1f\n",totalPrice());
     }
 
+    // 장바구니에 포함된 메뉴들의 금액 합산
     public double totalPrice() {
         double totalPrice = 0;
         for(MenuItem tp : getShoppingCart()) {
@@ -43,5 +46,4 @@ public class ShoppingCart {
         }
         return totalPrice;
     }
-
 }
